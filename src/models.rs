89bfd3f844fdd3f697a3a4bea8bf5ecbda667d85
zofiamilczarek <zmilcz@gmail.com>
2024-09-04 22:10:29 +0200

    added todos to the file

---
+++ src/models.rs
@@ -0,0 +1,6 @@
+// to implement:
+// - feedforward layer struct : an integrated weight + bias matrix, an activation function
+// - model struct : a (list?) of objects with the trait can_backprop (this can be layers, transformer blocks, embedding layers)
+// - activation function
+
+
