3d94d47eddac89fc45a8f54ff72652b75a367d91
Quentin Schroeder <quentinschroeder@Quentins-MacBook-Air.local>
2024-09-13 16:50:17 +0200

    commented the idea for implementing backprop

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 extern crate nalgebra as na;
 use na::{DMatrix, DVector};
 
+
 type Vector = DVector<f32>;
 type Matrix = DMatrix<f32>;
 
 struct Activation {
-    forward : fn (Vector) -> Vector,
-    backward : fn (Vector) -> Matrix
-} 
+    act_forward : fn (Vector) -> Vector,
+    act_backward : fn (Vector) -> Matrix
+}
 
 fn max(val : f32, comp : f32, out : f32, default : f32) -> f32 {
     if val >= comp {
@@ -32,39 +33,67 @@ fn drelu(v : Vector) -> Matrix {
     DMatrix::from_diagonal(&v.map(drelu_scalar))
 }
 
-struct Layer {
+struct Linear {
     bias : Vector,
-    weights : Matrix,
-    activation : Activation
+    weights : Matrix
 }
 
-fn make_standard_layer(bias : Vector, weights : Matrix) -> Layer {
-    Layer {
-	    bias,
-	    weights,
-	    activation : Activation {
-		forward : relu,
-		backward : drelu
-	    }
-	}
-}
+type Objective = ();
 
-pub trait Model {
+pub trait BProp {
     fn forward(&self,v : Vector) -> Vector;
     fn backward(&self, v : Vector) -> Matrix;
 }
 
-impl Model for Layer {
+impl BProp for Linear {
     fn forward(&self, v : Vector) -> Vector {
-	let linear_part = (self.weights.clone() * v.clone()) + self.bias.clone();
-	(self.activation.forward)(linear_part)
+	(self.weights.clone() * v.clone()) + self.bias.clone()
+    }
+
+    fn backward(&self, _v : Vector) -> Matrix {
+	self.weights.clone()
+    }
+}
+
+impl BProp for Activation {
+    fn forward(&self, v : Vector) -> Vector {
+	(self.act_forward)(v)
     }
 
     fn backward(&self, v : Vector) -> Matrix {
-	let linear_part = self.weights.clone() * v.clone() + self.bias.clone();
-	(self.activation.backward)(linear_part) * self.weights.clone()
+	(self.act_backward)(v)
     }
 }
+
+const RELU : Activation = Activation {
+    act_forward : relu,
+    act_backward : drelu
+};
+
+struct Model {
+    sequence : Vec<Box<dyn BProp>>,
+    objective : Objective
+}
+
+// Idea implement a mutable model with append, etc
+
+fn make_Model() {
+}
+ 
+
+impl BProp for Model {
+    fn forward(&self, v : Vector) -> Vector {
+	 (&self.sequence).into_iter().fold(v, |acc, f| f.forward(acc))
+    }
+
+    fn backward(&self, v : Vector) -> Matrix {
+	// IDEA:
+	// (ders, loss) = fs.fold(([],v), | (acc, val), f | (acc.unshift(f.backward(val), f.forward(val))))
+	// (totalders, _) = ders.fold(([], ID), |(acc, curr), d| let v = val * d in (acc.append(v), v)) 
+    }
+}
+
+
 
 fn main() {
     // A matrix with three lines and four columns.
